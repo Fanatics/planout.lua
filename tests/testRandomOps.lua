5279d5e16056ebfc49d3c1781abc12dcdc17aece
Brian Phipps <bgphipps@gmail.com>
2016-03-03 10:16:59 -0700

    removed commented out JS code, modified tests so they run and fail

--- tests/testRandomOps.lua
+++ tests/testRandomOps.lua
@@ -1,92 +1,56 @@
 package.path = package.path .. ";../src/?.lua;"
-require("resources.luaunit")
 require("ops.random")
 local pretty = require 'pl.pretty'
 -- included this for stuff like map, reduce, join.
 _ = require 'underscore'
+local Assignment = require "assignment"
+EXPORT_ASSERT_TO_GLOBALS = true
+require("resources.luaunit")
+
+TestRandomOps = {}
 
 local z = 3.29
 
--- function assertProp(observedP, expectedP, N) {
---   var se = z * Math.sqrt(expectedP * (1 - expectedP) / N);
---   expect(Math.abs(observedP - expectedP) <= se).toBe(true);
--- }
 function assertProp(observedP, expectedP, N)
   local se = z * math.sqrt(expectedP * (1 - expectedP) / N)
   assert((math.abs(observedP - expectedP) <= se) == true)
 end
---
--- function sum(obj) {
---   return obj.reduce(function(totalSum, cur) {
---     return totalSum + cur;
---   }, 0);
--- }
+
 function sum(obj)
   return _.map(obj, function(totalSum, cur) return totalSum + cur end)
 end
 
---
--- function valueMassToDensity(valueMass) {
---   var values = valueMass.map(function(val) { return Object.keys(val)[0]; });
---   var ns     = valueMass.map(function(val) { return val[Object.keys(val)[0]]});
---   var ns_sum = parseFloat(sum(ns));
---   ns         = ns.map(function(val) {
---     return val / ns_sum;
---   });
---   var ret = {};
---   for (var i = 0; i < values.length; i++) {
---     ret[values[i]] = ns[i];
---   }
---   return ret;
--- }
-
-function valueMassToDensity(valueMass) {
+function valueMassToDensity(valueMass)
   local values = _.map(valueMass, function(val) return val[1] end)
   local ns = _.map(valueMass, function(val) return val[val[1]] end)
   local ns_sum = sum(ns)
+  ns = _.map(ns, function(val) return val / ns_sum end)
   local ret = {}
-  for i=0, table.getn(values) do
+  for i=1, #values do
     ret[values[i]] = ns[i]
   end
-}
---
--- function Counter(l) {
---   var ret = {}
---   l.forEach(function(el) {
---     if (ret[el]) {
---       ret[el] += 1;
---     } else {
---       ret[el] = 1;
---     }
---   });
---   return ret;
--- }
+  return ret
+end
+
 function Counter(l)
   local ret = {}
   for k,v in pairs(l) do
-    if(ret[k]) then ret[k] += 1 else ret[k] = 1 end
+    if ret[k] then
+      ret[k] = ret[k] + 1
+    else
+      ret[k] = 1
+    end
   end
+  return ret
 end
---
--- function assertProbs(xs, valueDensity, N) {
---   var hist = Counter(xs);
---   Object.keys(hist).forEach(function(el) {
---     assertProp(hist[el] / N, valueDensity[el], N);
---   });
--- }
+
 function assertProbs(xs, valueDensity, N)
   local hist = Counter(xs)
   for k,v in pairs(hist) do
+    print(valueDensity[k])
     assertProp(hist[k] / N, valueDensity[k], N)
   end
 end
-
---
--- function distributionTester(xs, valueMass, N=10000) {
---   var valueDensity = valueMassToDensity(valueMass);
---
---   assertProbs(xs, valueDensity, parseFloat(N));
--- }
 
 function distributionTester(xs, valueMass, N)
   N = N or 10000
@@ -94,158 +58,74 @@ function distributionTester(xs, valueMass, N)
   assertProbs(xs, valueDensity, N)
 end
 
--- BEFORE --
--- beforeEach(() => {
---   ExperimentSetup.toggleCompatibleHash(true);
--- });
-
-function salts_correctly()
+function TestRandomOps:test_salts_correctly()
   local i = 20;
   local a = Assignment:new("assign_salt_a")
 
-  a:set('x', RandomInteger:new({ 'min': 0, 'max': 100000, 'unit': i}));
-  a:set('y', RandomInteger:new({ 'min': 0, 'max': 100000, 'unit': i}));
-  assert(a:get('x') != a:get('y'))
+  a:set('x', RandomInteger:new({['min'] = 0, ['max'] = 100000, ['unit'] = i }))
+  a:set('y', RandomInteger:new({['min'] = 0, ['max'] = 100000, ['unit'] = i }))
+  assert(a:get('x') ~= a:get('y'))
 
-  a:set('z', RandomInteger:new({ 'min': 0, 'max': 100000, 'unit': i, 'salt': 'x'}));
+  a:set('z', RandomInteger:new({ ['min'] = 0, ['max'] = 100000, ['unit'] = i, ['salt'] = 'x'}))
   assert(a:get('x') == a:get('z'));
 
-  var b = Assignment:new('assign_salt_b');
-  b:set('x', RandomInteger:new({ 'min': 0, 'max': 100000, 'unit': i}));
-  assert(a:get('x') != b:get('x'));
+  local b = Assignment:new('assign_salt_b');
+  b:set('x', RandomInteger:new({ ['min'] = 0, ['max'] = 100000, ['unit'] = i}));
+  assert(a:get('x') ~= b:get('x'));
 
-  a:set('f', RandomInteger:new({ 'min': 0, 'max': 100000, 'unit': i, 'full_salt':'fs'}));
-  b:set('f', RandomInteger:new({ 'min': 0, 'max': 100000, 'unit': i, 'full_salt':'fs'}));
+  a:set('f', RandomInteger:new({ ['min'] = 0, ['max'] = 100000, ['unit'] = i, ['full_salt'] ='fs'}))
+  b:set('f', RandomInteger:new({ ['min'] = 0, ['max'] = 100000, ['unit'] = i, ['full_salt'] ='fs'}))
   assert(a:get('f') == b:get('f'));
 
-  a:set('f', RandomInteger:new({ 'min': 0, 'max': 100000, 'unit': i, 'full_salt':'fs2'}));
-  b:set('f', RandomInteger:new({ 'min': 0, 'max': 100000, 'unit': i, 'full_salt':'fs2'}));
+  a:set('f', RandomInteger:new({ ['min'] = 0, ['max'] = 100000, ['unit'] = i, ['full_salt'] ='fs2'}))
+  b:set('f', RandomInteger:new({ ['min'] = 0, ['max'] = 100000, ['unit'] = i, ['full_salt'] ='fs2'}))
   assert(a:get('f') == b:get('f'));
 end
 
-function works_for_bernoulli_trials()
-  -- var N = 10000;
-  -- function bernoulli(p) {
-  --   var xs = [];
-  --   for (var i = 0; i < N; i++) {
-  --     var a = new Assignment(p);
-  --     a.set('x', new Random.BernoulliTrial({ 'p': p, 'unit': i }));
-  --     xs[i] = a.get('x');
-  --   }
-  --   return xs;
-  -- }
-  --
-  -- ExperimentSetup.toggleCompatibleHash(true);
-  -- distributionTester(bernoulli(0.0), [{0: 1}, {1: 0}], N);
-  -- distributionTester(bernoulli(0.1), [{0: 0.9}, {1: 0.1}], N);
-  -- distributionTester(bernoulli(1.0), [{0: 0}, {1: 1}], N);
-  -- ExperimentSetup.toggleCompatibleHash(false);
-  -- distributionTester(bernoulli(0.0), [{0: 1}, {1: 0}], N);
-  -- distributionTester(bernoulli(0.1), [{0: 0.9}, {1: 0.1}], N);
-  -- distributionTester(bernoulli(1.0), [{0: 0}, {1: 1}], N);
-
-
+function TestRandomOps:test_works_for_bernoulli_trials()
   local N = 10000
   function bernoulli(p)
     local xs = {}
     for i=0, N do
       local a = Assignment:new(p)
-      a:set('x', BernoulliTrial:new({'p': p, 'unit': i }))
-      xs[i] = a.get('x')
-    end
-    return xs
-  end
-
-  -- Waiting for expirmentsetup to be implemented
-  -- ExperimentSetup.toggleCompatibleHash(true);
-  distributionTester(bernoulli(0.0), {{0: 1}, {1: 0}}, N);
-  distributionTester(bernoulli(0.1), {{0: 0.9}, {1: 0.1}}, N);
-  distributionTester(bernoulli(1.0), {{0: 0}, {1: 1}}, N);
-  -- ExperimentSetup.toggleCompatibleHash(false);
-  distributionTester(bernoulli(0.0), {{0: 1}, {1: 0}}, N);
-  distributionTester(bernoulli(0.1), {{0: 0.9}, {1: 0.1}}, N);
-  distributionTester(bernoulli(1.0), {{0: 0}, {1: 1}], N};
-
-end
-
-function works_for_uniform_choice()
-  -- var N = 10000;
-  -- function uniformChoice(choices) {
-  --   var xs = [];
-  --   for (var i = 0; i < N; i++) {
-  --     var a = new Assignment(choices.join(','));
-  --     a.set('x', new Random.UniformChoice({ 'choices': choices, 'unit': i }));
-  --     xs[i] = a.get('x');
-  --   }
-  --   return xs;
-  -- }
-  --
-  -- function testDistributions() {
-  --   distributionTester(uniformChoice(['a']), [{'a': 1}], N);
-  --   distributionTester(uniformChoice(['a', 'b']), [{'a': 1}, {'b': 1}], N);
-  -- };
-  --
-  -- ExperimentSetup.toggleCompatibleHash(true);
-  -- testDistributions();
-  --
-  -- ExperimentSetup.toggleCompatibleHash(false);
-  -- testDistributions();
-  local N = 10000
-  function uniformChoice(choices)
-    local xs = {}
-    for i=0, N do
-      local a = Assignment:new(_.join(choices, ','))
-      a:set('x', UniformChoice:new({ 'choices', choices, 'unit': i}))
+      a:set('x', BernoulliTrial:new({['p'] = p, ['unit'] = i }))
       xs[i] = a:get('x')
     end
     return xs
   end
 
-  function testDistributions() {
-    distributionTester(uniformChoice({'a'}), {{'a': 1}}, N);
-    distributionTester(uniformChoice({'a', 'b'}), {{'a': 1}, {'b': 1}}, N);
-  };
+  distributionTester(bernoulli(0.0), {{[0] = 1}, {[1] = 0}}, N)
+  distributionTester(bernoulli(0.1), {{[0] = 0.9}, {[1] = 0.1}}, N)
+  distributionTester(bernoulli(1.0), {{[0] = 0}, {[1] = 1}}, N)
+  distributionTester(bernoulli(0.0), {{[0] = 1}, {[1] = 0}}, N)
+  distributionTester(bernoulli(0.1), {{[0] = 0.9}, {[1] = 0.1}}, N)
+  distributionTester(bernoulli(1.0), {{[0] = 0}, {[1] = 1}, N})
 
-  -- ExperimentSetup.toggleCompatibleHash(true);
+end
+
+function TestRandomOps:works_for_uniform_choice()
+  local N = 10000
+  function uniformChoice(choices)
+    local xs = {}
+    for i=0, N do
+      local a = Assignment:new(_.join(choices, ','))
+      a:set('x', UniformChoice:new({ ['choices'] = choices, ['unit'] = i}))
+      xs[i] = a:get('x')
+    end
+    return xs
+  end
+
+  function testDistributions()
+    distributionTester(uniformChoice({'a'}), {{['a'] = 1}}, N);
+    distributionTester(uniformChoice({'a', 'b'}), {{['a'] = 1}, {['b'] = 1}}, N);
+  end
+
   testDistributions();
-
-  -- ExperimentSetup.toggleCompatibleHash(false);
   testDistributions();
 
 end
 
-function works_for_weighted_choice()
-  -- var N = 10000;
-  -- function weightedChoice(choices) {
-  --   var xs = [];
-  --   var weights = choices.map(function(choice) { return choice[Object.keys(choice)[0]]});
-  --   var choices = choices.map(function(choice) { return Object.keys(choice)[0]; });
-  --   for (var i = 0; i < N; i++) {
-  --     var a = new Assignment(weights.join(', '));
-  --     a.set('x', new Random.WeightedChoice({ 'choices': choices, 'weights': weights, 'unit': i }));
-  --     xs[i] = a.get('x');
-  --   }
-  --   return xs;
-  -- }
-  --
-  -- function testDistributions() {
-  --   var d = [{'a': 1}];
-  --   distributionTester(weightedChoice(d), d, N);
-  --   d = [{'a': 1}, {'b': 2}];
-  --   distributionTester(weightedChoice(d), d, N);
-  --   d = [{'a': 0}, {'b': 2}, {'c': 0}];
-  --   distributionTester(weightedChoice(d), d, N);
-  --
-  --   var da = [{'a': 1}, {'b': 2}, {'c': 0}, {'a': 2}];
-  --   var db = [{'a': 3}, {'b': 2}, {'c': 0}];
-  --   distributionTester(weightedChoice(da), db, N);
-  -- }
-  --
-  -- ExperimentSetup.toggleCompatibleHash(true);
-  -- testDistributions();
-  --
-  -- ExperimentSetup.toggleCompatibleHash(false);
-  -- testDistributions();
+function TestRandomOps:test_works_for_weighted_choice()
   local N = 10000
   function weightedChoice(choices)
     local xs = {}
@@ -253,86 +133,37 @@ function works_for_weighted_choice()
     local choices = _.map(choices, function(choice) return choice[1] end)
     for i=0, N do
       local a = Assignment:new(_.join(weights, ', '))
-      a:set('x', WeightedChoice:new({'choices': choices, 'weights': weights, 'unit': i }))
+      a:set('x', WeightedChoice:new({['choices'] = choices, ['weights'] = weights, ['unit'] = i }))
       xs[i] = a.get('x')
     end
     return xs
   end
 
-  function testDistributions() {
-    var d = {{'a': 1}};
-    distributionTester(weightedChoice(d), d, N);
-    d = {{'a': 1}, {'b': 2}};
-    distributionTester(weightedChoice(d), d, N);
-    d = {{'a': 0}, {'b': 2}, {'c': 0}};
-    distributionTester(weightedChoice(d), d, N);
+  function testDistributions()
+    local d = {{['a'] = 1}}
+    distributionTester(weightedChoice(d), d, N)
+    d = {{['a'] = 1}, {['b'] = 2}}
+    distributionTester(weightedChoice(d), d, N)
+    d = {{['a'] = 0}, {['b'] = 2}, {['c'] = 0}}
+    distributionTester(weightedChoice(d), d, N)
 
-    var da = {{'a': 1}, {'b': 2}, {'c': 0}, {'a': 2}};
-    var db = {{'a': 3}, {'b': 2}, {'c': 0}};
-    distributionTester(weightedChoice(da), db, N);
-  }
+    local da = {{['a'] = 1}, {['b'] = 2}, {['c'] = 0}, {['a'] = 2}}
+    local db = {{['a'] = 3}, {['b'] = 2}, {['c'] = 0}}
+    distributionTester(weightedChoice(da), db, N)
+  end
 
-  ExperimentSetup.toggleCompatibleHash(true);
   testDistributions();
 
-  ExperimentSetup.toggleCompatibleHash(false);
   testDistributions();
 end
 
-function works_for_sample()
-  -- var N = 100;
-  -- function sample(choices, draws) {
-  --   var xs = [];
-  --   for (var i = 0; i < N; i++) {
-  --     var a = new Assignment(choices.join(', '));
-  --     a.set('x', new Random.Sample({ 'choices': choices, 'draws': draws, 'unit': i }));
-  --     xs[i] = a.get('x');
-  --   }
-  --   return xs;
-  -- }
-  --
-  -- function listDistributionTester(xsList, valueMass, N) {
-  --   var valueDensity = valueMassToDensity(valueMass);
-  --   var l = [];
-  --
-  --   /* bad equivalent to zip() from python */
-  --   xsList.forEach(function(xs, i){
-  --     xs.forEach(function(x, j) {
-  --       if (!l[j]) {
-  --         l[j] = [x];
-  --       } else {
-  --         l[j].push(x);
-  --       }
-  --     });
-  --     if (i === xsList.length-1) {
-  --       l.forEach(function(el) {
-  --         assertProbs(el, valueDensity, N);
-  --       });
-  --     }
-  --   });
-  -- }
-  --
-  -- function testDistributions() {
-  --   var a = [1, 2, 3];
-  --   var ret = [{1: 1}, {2: 1}, {3: 1}];
-  --   listDistributionTester(sample(a, 3), ret, N);
-  --   listDistributionTester(sample(a, 2), ret, N);
-  --   a = ['a', 'a', 'b'];
-  --   ret = [{'a': 2}, {'b': 1}];
-  --   listDistributionTester(sample(a, 3), ret, N);
-  -- }
-  --
-  -- ExperimentSetup.toggleCompatibleHash(true);
-  -- testDistributions();
-  --
-  -- ExperimentSetup.toggleCompatibleHash(false);
-  -- testDistributions();
+function TestRandomOps:test_works_for_sample()
   local N = 100
   function sample(choices, draws)
     local xs = {}
     for i=0, N do
       local A = Assignment:new(_.join(choices, ', '))
-      a:set('x', Sample:new({'choices': choices, 'draws': draws, 'unit': i}))
+      a:set('x', Sample:new({['choices'] = choices, ['draws'] = draws, ['unit'] = i}))
       xs[i] = a.get('x')
     end
     return xs
@@ -342,16 +173,16 @@ function works_for_sample()
     local valueDensity = valueMassToDensity(valueMass)
     local l = {}
 
-    for xs,i in pairs(xsList)
-      for x,j in pairs(key)
-        if !l[j] then
-          l[j] = [k]
+    for xs,i in pairs(xsList) do
+      for x,j in pairs(key) do
+        if l[j] then
+          l[j] = k
         else
           table.insert(l[j], k)
         end
       end
-      if i == table.getn(xsList)
-        for k,v in pairs(l)
+      if i == table.getn(xsList) then
+        for k,v in pairs(l) do
           assertProbs(el, valueDensity, N)
         end
       end
@@ -360,27 +191,27 @@ function works_for_sample()
 
   function testDistributions()
     local a = {1,2,3}
-    local ret = {{1:1},{2:1},{3:1}}
+    local ret = {{[1] = 1},{[2] = 1},{[3] = 1}}
     listDistributionTester(sample(a, 2), ret, N)
     listDistributionTester(sample(a, 2), ret, N)
     a = {'a', 'a', 'b'}
-    ret = {{'a': 2}, {'b': 1}}
+    ret = {{['a'] = 2}, {['b'] = 1}}
     listDistributionTester(sample(a, 3), ret, N)
   end
-  ExperimentSetup.toggleCompatibleHash(true);
   testDistributions();
 
-  ExperimentSetup.toggleCompatibleHash(false);
   testDistributions();
 end
 
-function works_for_efficient_sample()
-  -- ExperimentSetup.toggleCompatibleHash(false);
-  -- var choices = [1, 2, 3, 4, 5, 6, 7];
-  -- var draws = 5;
-  --
-  -- var a = new Assignment(choices.join(', '));
-  -- a.set('x', new Random.Sample({ 'choices': choices, 'draws': draws, 'unit': '1' }));
-  -- var x = a.get('x');
-  -- expect(x.length).toEqual(5);
+function TestRandomOps:test_works_for_efficient_sample()
+  local choices = {1,2,3,4,5,6,7}
+  local draws = 5
+
+  local a = Assignment:new(_.join(choices, ', '))
+  a.set('x', Sample:new({['choices'] = choices, ['draws'] = draws, ['unit'] = '1'}))
+  local x = a:get('x')
+  assert(table.getn(x) == 5)
 end
+
+local lu = LuaUnit.new()
+os.exit( lu:runSuite() )

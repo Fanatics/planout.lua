1ebb97f7ef033174d0a4a525c603f69950f0193f
mgloystein <mgloystein@fanatics.com>
2016-03-23 14:41:21 -0600

    Fixed somethings

--- dist/OpWriters.js
+++ dist/OpWriters.js
@@ -8,13 +8,13 @@ class OpWriter {
     if(typeof(luaClass) === "function") {
       this.luaWriter = new luaClass(settings)
     } else if(typeof(luaClass) === "string") {
-      this.luaWriter = new LuaCodeFinder(settings, luaClass)
+      this.luaWriter = new LuaOpWriter(settings, luaClass)
     }
   }
 
-  write(buffer, children) {
+  write(buffer) {
     this.beginWrite(buffer)
-    children.forEach((child) => child.write(buffer))
+
     this.endWrite(buffer)
   }
 
@@ -24,6 +24,16 @@ class OpWriter {
 
   endWrite(buffer) {
 
+  }
+
+  setProp(name, value) {
+    if(this.properties === undefined) {
+      this.properties = {}
+    }
+    if(this.properties[name] === undefined) {
+      this.properties[name] = value
+    }
+    this.luaWriter.setProp(name, value)
   }
 }
 
@@ -40,28 +50,46 @@ class SetWriter extends OpWriter {
       var prop = this.settings.var
       var val = JSON.stringify(this.settings.value)
       buffer.push( `my_input['${prop}'] = ${val}`)
+    } else {
+      this.properties.value.write(buffer)
     }
   }
 }
 
 class SeqOpWriter extends OpWriter {
 
+  beginWrite(buffer) {
+    let children = this.properties["seq"]
+    if(children !== undefined && children.length > 0) {
+      children.forEach((child) => {
+        if(typeof(child.write) === "function") {
+          child.write(buffer)
+        }
+      })
+    }
+  }
 }
 
 class CondOpWriter extends OpWriter {
   constructor(settings, luaClass) {
     super(settings, luaClass)
-    this.instanceId = CondOpWriter.instance++
   }
 
 
   beginWrite(buffer) {
-    var id = this.instanceId
-    var result = `my_input['specific_goal']`
-    buffer.push(`
-      
+    let conditions = this.properties.cond
+    conditions.forEach((cond) => {
+      let id = CondOpWriter.instance++
+      let buffTemp = []
+      cond.ifCond.writer.write(buffTemp)
+      let result = buffTemp.join("")
+      buffer.push(`
+
     local cond_${id} = ${result}
     if cond_${id} and cond_${id} ~=0 then`)
+
+      cond.thenAction.writer.write(buffer)
+    })
   }
 
   endWrite(buffer) {
@@ -75,23 +103,57 @@ CondOpWriter.instance = 0
 class LuaCodeWriter {
   constructor(settings, luaOp) {
     this.luaOp = luaOp
+    this.properties = {}
+  }
+
+  setProp(name, value) {
+    if(this.properties[name] === undefined) {
+      this.properties[name] = value
+    }
   }
 
   writeStandard(variable, childList, needsExecuting) {
-
   }
 }
 
-class LuaCodeFinder extends LuaCodeWriter {
+class LuaOpWriter extends LuaCodeWriter {
 
-
-  evaluateChildren(childList) {
-
+  evalProp(prop) {
+    let val = this.properties[prop]
+    let tempBuff = []
+    tempBuff.push(`['${prop}'] = `)
+    if(typeof(val) === "object" &&
+      typeof(val.write) === "function" ) {
+      val.write(tempBuff)
+    } else if(val instanceof Array) {
+      tempBuff.push("{", val.map(this.evalArrProp.bind(this)), "}")
+    } else {
+      tempBuff.push(val)
+    }
+    return tempBuff.join("")
   }
 
-  writeStandard(variable, childList, needsExecuting) {
+  evalArrProp(val) {
+    let tempBuff = []
+    if(typeof(val) === "object" &&
+      typeof(val.write) === "function" ) {
+      val.write(tempBuff)
+    } else if(val instanceof Array) {
+      return val.map(this.evalArrProp.bind(this))
+    } else {
+      tempBuff.push(val)
+    }
+    return tempBuff.join("")
+  }
+
+  evaluateChildren() {
+    var buff = Object.keys(this.properties).map(this.evalProp.bind(this))
+    return buff.join(", ")
+  }
+
+  writeStandard(variable, needsExecuting) {
     const operation = this.luaOp
-    const children = this.evaluateChildren(childList)
+    const children = this.evaluateChildren()
     return `
 
     assigment:set('${variable}', ${operation}:new({
@@ -100,4 +162,4 @@ class LuaCodeFinder extends LuaCodeWriter {
     my_input['${variable}'] = assigment:get('${variable}')` + (needsExecuting ? `:simpleExecute()` : ``)
   }
 }
-module.exports = { OpWriter, GetWriter, SeqOpWriter, CondOpWriter, LuaCodeWriter }
+module.exports = { OpWriter, GetWriter, SeqOpWriter, CondOpWriter, LuaCodeWriter, SetWriter }
